196702de87322b2f2ac53e259a9e0c4831f52b07
Clademir Zampieri <clademir.zampieri@ndd.tech>
2024-07-06 17:04:27 -0300

    Incluídos arquivos faltantes

---
+++ TechChallenge_1/Testes/TechChallenge_1.Domain.Test/RemoverContato/RemoverContatoPersistirDadosImplTests.cs
@@ -0,0 +1,67 @@
+﻿using Moq;
+
+using NUnit.Framework;
+
+using TechChallenge_1.Domain.Features.RemoverContato.Contexto;
+using TechChallenge_1.Domain.Features.RemoverContato.Modulos.PersistirDados;
+using TechChallenge_1.Domain.Features.RemoverContato.Requisicao;
+using TechChallenge_1.Domain.Repositorio;
+
+namespace TechChallenge_1.Domain.Test.RemoverContato
+{
+    [TestFixture]
+    public class PersistirDadosRemoverContatoImplTests
+    {
+        private Mock<IRepositorioContato> _repositorioMock;
+        private PersistirDadosRemoverContatoImpl _persistirDados;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _repositorioMock = new Mock<IRepositorioContato>();
+            _persistirDados = new PersistirDadosRemoverContatoImpl(_repositorioMock.Object);
+        }
+
+        [Test]
+        public void Processar_ValidContext_ReturnsTrue()
+        {
+            // Arrange
+            var contexto = new ContextoRemoverContato
+            {
+                Requisicao = new RequisicaoRemoverContato
+                {
+                    IdContato = Guid.NewGuid()
+                }
+            };
+
+            _repositorioMock.Setup(r => r.RemoverContatoAsync(It.IsAny<Guid>())).ReturnsAsync(true);
+
+            // Act
+            var result = _persistirDados.Processar(contexto);
+
+            // Assert
+            Assert.IsTrue(result);
+        }
+
+        [Test]
+        public void Processar_ValidContext_ReturnsFalse()
+        {
+            // Arrange
+            var contexto = new ContextoRemoverContato
+            {
+                Requisicao = new RequisicaoRemoverContato
+                {
+                    IdContato = Guid.NewGuid()
+                }
+            };
+
+            _repositorioMock.Setup(r => r.RemoverContatoAsync(It.IsAny<Guid>())).ReturnsAsync(false);
+
+            // Act
+            var result = _persistirDados.Processar(contexto);
+
+            // Assert
+            Assert.IsFalse(result);
+        }
+    }
+}
